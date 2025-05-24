f867b5eec40493d564883ef5e1ae82805d73ce22
vikhyaat12 <vikhyaat.sikand@gmail.com>
2025-05-24 17:43:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      external: ['index-IZ09xKqW.js']
-    }
+    outDir: 'dist'
   }
 });
