ad068d98d046332655f49273b346dc7a3a14aed3
vikhyaat12 <vikhyaat.sikand@gmail.com>
2025-05-24 17:50:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import data from "./data.json";
 import LanguageSwitcher from "./components/LanguageSwitcher";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
-import ServicesSection from "./components/ServicesSection";
 import StorySection from "./components/StorySection";
+import ServicesSection from "./components/ServicesSection";
 import ContactSection from "./components/ContactSection";
-import "./main.css";
 
 export default function App() {
-  const [language, setLanguage] = useState(
-    (navigator.language || navigator.userLanguage).startsWith("hi") ? "hi" : "en"
-  );
+  const [lang, setLang] = useState("en");
+
+  const content = data[lang];
+
   return (
-    <>
-      <LanguageSwitcher language={language} setLanguage={setLanguage} />
-      <HeroSection data={data.hero[language]} />
-      <AboutSection data={data.about[language]} />
-      <StorySection data={data.story[language]} />
-      <ServicesSection data={data.services[language]} />
-      <ContactSection data={data.contact[language]} />
-    </>
+    <div className="container">
+      <LanguageSwitcher lang={lang} setLang={setLang} />
+      <HeroSection data={content.hero} />
+      <AboutSection data={content.about} />
+      <StorySection data={content.story} />
+      <ServicesSection data={content.services} />
+      <ContactSection data={content.contact} />
+    </div>
   );
 }
