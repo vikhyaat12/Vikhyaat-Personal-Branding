70716a3c2f991cb93ce42d728e65dd289b65aaa3
vikhyaat12 <vikhyaat.sikand@gmail.com>
2025-05-24 13:47:15 +0530

    Delete src_components_AboutSection.jsx

--- src_components_AboutSection.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function AboutSection({ data }) {
-  return (
-    <section className="about">
-      <h2>{data.title}</h2>
-      <p>{data.body}</p>
-    </section>
-  );
-}
