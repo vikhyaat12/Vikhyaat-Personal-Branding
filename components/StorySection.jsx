09db33aa1f5bdc8d3768e55cab6337450a97ee24
vikhyaat12 <vikhyaat.sikand@gmail.com>
2025-05-28 00:46:49 +0530

    Delete components directory

--- components/StorySection.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function StorySection({ data }) {
-  return (
-    <section className="story">
-      <h2>{data.title}</h2>
-      <p>{data.body}</p>
-    </section>
-  );
-}
