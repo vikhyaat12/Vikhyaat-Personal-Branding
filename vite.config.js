78bcadde85948a674cba4946ff731744e0a0149f
vikhyaat12 <vikhyaat.sikand@gmail.com>
2025-06-16 23:15:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'images', // ✅ Serve the /images folder as static
   build: {
     outDir: 'dist'
   }
